971293bea5a3f3aad0b7342c5623e0fac8c8826a
Pavankumar Laxman Chandankar <69724969+pavan1609@users.noreply.github.com>
2024-04-21 22:12:13 +0200

    velocity_profile_solver_2D_with_const_.py

---
+++ velocity_profile_solver_2D_with_const_.py
@@ -0,0 +1,52 @@
+
+#Python program to solve the system of linear equations obtained from difference equations to solve for 𝒖𝒛 as a function of r and plot the 𝒖𝒛 as a function of r to visualize the velocity profile in the pipe for the boundary condition and parameters given.
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Parameters
+mu = 0.01   #Viscosity of fluid
+R = 0.05    #Radius of Pipe
+N = 100     #Grid Points
+delta_r = R / N
+
+#Initialisation for Velocity Vector u_z, Cofficeint Matrix A for equation, Constant Vector b
+u_z = np.zeros(N)
+A = np.zeros((N, N))
+b = np.zeros(N)
+
+#Creating radius values into array at different points
+r_values = np.linspace(0, R, N)
+
+# Initial Boundary conditions
+A[0, 0] = 1.0   # Boundary condition at r = 0
+A[0, 1] = -1.0  # Boundary condition at r = 0
+b[0] = 0        # Boundary condition at r = 0
+
+# Final Boundary Conditions
+A[-1, -1] = 1  #Boundary Condition at r = R
+b[-1] = 0      #Boundary Condition at r = R
+
+#Forming Coefficient matrix from the equation using for loop
+for i in range(1, N-1):
+    A[i, i-1] = (mu / (delta_r**2)) * r_values[i]                  #coefficient of u at i-1
+    A[i, i] = -(mu / (delta_r**2)) * ((2 * r_values[i]) + delta_r) #coefficient of u at i
+    A[i, i+1] = (mu / (delta_r**2)) * (r_values[i] + delta_r)      #coefficient of u at i+1
+     
+#Forming Constant Vector matrix from equation using for loop
+for i in range(1, N-1):
+    b[i] = -100 * r_values[i]
+    
+#Function to solve the linear equations
+u_z = np.linalg.solve(A, b)
+#print(u_z)
+#print(r_values)
+
+#plotting the graph of velocity values at each radius point
+plt.plot(r_values, u_z)
+plt.xlabel('radius of pipe (in meters)',fontsize=12)
+plt.ylabel('Velocity of fluid  U_z (in m/s)',fontsize=12)
+plt.title('Velocity profile at each point on Radius',fontsize=12)
+plt.grid()
+plt.show()
